27d67846b0faa86ab95145f0846e2c3982c6bccb
Abhishek <abhishekchandra3001@gmail.com>
2025-01-30 09:53:15 +0530

    FIX A Server error when login in

--- backend/models/User.js
+++ backend/models/User.js
@@ -60,4 +60,13 @@ userSchema.pre('save', async function(next) {
   }
 });
 
+// Method to compare password for login
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = mongoose.model('User', userSchema);
